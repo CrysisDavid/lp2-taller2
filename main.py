17e29feb78fe51a93cfd09c43d4b11477cc7ccf0
david <davidsoliss123@gmail.com>
2025-03-28 02:30:41 -0500

    final details, fonts, colors and animations

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect # type: ignore
+from flask import Flask, render_template, redirect  # type: ignore
 import sqlite3
 from pprint import pprint
 
@@ -7,22 +7,28 @@ conexion = sqlite3.connect("web2.sqlite3")
 conexion.row_factory = sqlite3.Row
 cursor = conexion.cursor()
 cursor.execute("SELECT * FROM products;")
-productos = [dict(p) for p in cursor.fetchall()]
+productos = [dict(producto) for producto in cursor.fetchall()]
 cursor.close()
 conexion.close()
 
 # aplicación
 app = Flask(__name__)
 
-# rutas
-@app.route('/')
-def ruta_raiz():
-  return render_template('index.html', productos = productos)
 
-@app.route('/producto/<int:pid>')
+# rutas
+@app.route("/")
+def ruta_raiz():
+    return render_template("index.html", productos=productos)
+
+
+@app.route("/producto/<int:pid>")
 def ruta_producto(pid):
-  pass
-  
+    for producto in productos:
+        if pid == producto["id"]:
+            return render_template("producto.html", producto=producto)
+    return redirect("/")
+
+
 # programa principal
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
